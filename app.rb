1c15c987aba1e78d376d5b05f7751a3c2e6fed96
Ethan Sagin <ethansagin@gmail.com>
2019-06-07 14:27:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,9 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-configure do
-  enable :sessions
-  set :session_secret, "secret"
-end
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    
+    erb :index
+    
+  end
 
 end
